9047943077c351f9d620dcd10122755094a8cf6a
Zaina <159014285+zainazakker@users.noreply.github.com>
2024-08-09 12:24:15 +0530

    Add files via upload

---
+++ num.py
@@ -0,0 +1,13 @@
+import numpy as np
+ar1=np.array([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]])
+print(ar1)
+print("display all elements excluding first row:")
+print(ar1[1:,:])
+print("display all elements excluding last column:")
+print(ar1[:,:3])
+print("display all elements 1st and 2nd column in 2nd and 3rd row:")
+print(ar1[1:3,0:2])
+print("display all elements 2st and 3rd column ")
+print(ar1[:,1:3])
+print("display elements 2nd and 3rd element of 1 row:")
+print(ar1[:1,1:3])
